280666740b090af0ebb96a8f368d579e3c232f5a
Pedro Moacyr Barcelos Neto <pedro@barcelosneto.com>
2021-03-09 06:49:13 -0400

    Adicionar arquivos de projeto.

---
+++ WebApplication4/Book.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication4
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Specifications Specifications { get; set; }
+
+        public List<Book> ListarBooks()
+        {
+            var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"Base.json");
+            var ListaBooks = JsonConvert.DeserializeObject<List<Book>>(json);
+           
+            return ListaBooks;
+        }
+    }
+}
---
+++ WebApplication4/Specifications.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4
+{
+    public class Specifications
+    {
+        public string Originallypublished { get; set; }
+        public string Author { get; set; }
+        public int Pagecount { get; set; }
+        public List<string> Illustrator { get; set; }
+        public List<string> Genres { get; set; }
+    }
+}
